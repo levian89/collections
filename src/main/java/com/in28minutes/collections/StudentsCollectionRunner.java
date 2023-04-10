aff98e9a2372f569273bbcc7b74dbad736ef4266
levian89 <levi.gabrian@gmail.com>
2023-04-10 19:37:07 +0300

    used a Comparable interface implementation

--- src/main/java/com/in28minutes/collections/StudentsCollectionRunner.java
+++ src/main/java/com/in28minutes/collections/StudentsCollectionRunner.java
@@ -1,9 +1,24 @@
 package com.in28minutes.collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class StudentsCollectionRunner
 {
     public static void main( String[] args )
     {
-        
+        List<Student> students = List.of(new Student(1,"Ranga"),
+                new Student(100, "Adam"),
+                new Student(2, "Eve"));
+
+        List<Student> studentsAl = new ArrayList<>(students);
+        //System.out.println(students);
+
+        System.out.println(studentsAl);
+
+        Collections.sort(studentsAl);
+
+        System.out.println(studentsAl);
     }
 }
